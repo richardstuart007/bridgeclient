0baf5123d28c2d9cf58582f2e0cec397c5a1eb7c
Richard <richardstuart007@hotmail.com>
2023-05-20 13:30:13 +1200

    debug fix

--- src/debug/debugSettings.js
+++ src/debug/debugSettings.js
@@ -1,17 +1,17 @@
-//
-//  Default values
-//
-let DEBUG_LOG_OVERRIDE = true
-let DEBUG_LOG = true
-//
-//  Application Environment Variables
-//
-const App_Env = JSON.parse(sessionStorage.getItem('App_Env'))
-if (App_Env) {
-  DEBUG_LOG_OVERRIDE = App_Env.DEBUG_LOG_OVERRIDE
-  DEBUG_LOG = App_Env.DEBUG_LOG
-}
 export default function debugSettings(debug = false) {
+  //
+  //  Default values
+  //
+  let DEBUG_LOG_OVERRIDE = false
+  let DEBUG_LOG = false
+  //
+  //  Application Environment Variables
+  //
+  const App_Env = JSON.parse(sessionStorage.getItem('App_Env'))
+  if (App_Env) {
+    DEBUG_LOG_OVERRIDE = App_Env.DEBUG_LOG_OVERRIDE
+    DEBUG_LOG = App_Env.DEBUG_LOG
+  }
   if (DEBUG_LOG_OVERRIDE) return DEBUG_LOG
   //
   // No Override - return incomming parameter (or default of false)
